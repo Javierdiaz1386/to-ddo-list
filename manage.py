e6854573e08f6b80af170cc1b3dbaaefe664b8e4
Javierdiaz1386 <javidavi16dd@gmail.com>
2023-07-09 09:44:25 -0500

    Configuracion vercel

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Django's command-line utility for administrative tasks.
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoList.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -20,3 +21,15 @@ def main():
 
 if __name__ == '__main__':
     main()
+"""
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoList.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError("No se pudo importar Django. Asegúrate de tenerlo instalado.") from exc
+    execute_from_command_line(sys.argv)
